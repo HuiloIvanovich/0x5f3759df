ef501d0a5adec956d1a4e594c6b903cfc340ace0
HuiloIvanovich <alex4152001@mail.ru>
2019-09-27 23:46:00 +0300

    fixed heroku deploy

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../config/db');
+const path = require('path');
 
 const users = require('./routes/api/users');
 
@@ -13,18 +14,18 @@ db();
 
 app.use('/api/users', users);
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('../client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-    });
-} else {
+// if (process.env.NODE_ENV === 'production') {
+//     //Set static folder
+//     app.use(express.static('client/build'));
+//
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+//     });
+// } else {
     app.get('/', (req, res) => {
         res.send('On server')
     });
-}
+//}
 
 const PORT = process.env.PORT || 5000;
 
