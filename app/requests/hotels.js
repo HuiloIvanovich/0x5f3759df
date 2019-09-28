5eeced8e75aa93910add5cdf317155c960424502
Masha Samsikova <41171543+holounic@users.noreply.github.com>
2019-09-28 09:42:19 +0300

    weather requests, coords needed for weather req

--- app/requests/hotels.js
+++ app/requests/hotels.js
@@ -1,9 +1,9 @@
 const axios = require("axios")
-const { KEY } = require("../../config")
+const { ATOKEN } = require("../../config")
 
 function getHotelByDate(checkIn = null, checkOut = null, location = null, currency = "rub", limit = 10) {
     return new Promise((resolve, reject) => {
-        axios.get(`http://engine.hotellook.com/api/v2/cache.json?location=${location}&checkIn=${checkIn}&checkOut=${checkOut}&currency=${currency}&limit=${limit}&token=321d6a221f8926b5ec41ae89a3b2ae7b`)
+        axios.get(`http://engine.hotellook.com/api/v2/cache.json?location=${location}&checkIn=${checkIn}&checkOut=${checkOut}&currency=${currency}&limit=${limit}&token=${ATOKEN}`)
         .then(response => {
             (response.length == 0) ? reject("check input") : resolve(response)
         })
